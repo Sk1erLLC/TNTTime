3caafbdca1e9583e493facb98e88a6c01b2d8afb
asbyth <cyronize@gmail.com>
2021-05-25 07:44:06 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/mods/tnttimer/tweaker/TNTTimerTweaker.java
+++ src/main/java/club/sk1er/mods/tnttimer/tweaker/TNTTimerTweaker.java
@@ -1,10 +1,10 @@
 package club.sk1er.mods.tnttimer.tweaker;
 
 import club.sk1er.mods.tnttimer.forge.FMLLoadingPlugin;
-import gg.essential.loader.EssentialTweaker;
+import gg.essential.loader.EssentialSetupTweaker;
 
 @SuppressWarnings("unused")
-public class TNTTimerTweaker extends EssentialTweaker {
+public class TNTTimerTweaker extends EssentialSetupTweaker {
     public TNTTimerTweaker() {
         super(new String[]{FMLLoadingPlugin.class.getName()});
     }
