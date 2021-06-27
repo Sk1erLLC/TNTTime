56e9595bd14c73e3b3f29a1ca28df3f75bed689a
asbyth <cyronize@gmail.com>
2021-06-27 09:00:12 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/mods/tnttimer/TNTTime.java
+++ src/main/java/club/sk1er/mods/tnttimer/TNTTime.java
@@ -9,7 +9,6 @@ import net.minecraft.client.renderer.entity.RenderTNTPrimed;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import org.lwjgl.opengl.GL11;
 
 import java.awt.Color;
@@ -22,22 +21,18 @@ public class TNTTime {
     public static TNTTime instance;
 
     private final Minecraft mc = Minecraft.getMinecraft();
+    private final DecimalFormat format = new DecimalFormat("0.00");
 
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-    }
-
+    @SuppressWarnings("unused")
     public void renderTag(RenderTNTPrimed tntRenderer, EntityTNTPrimed tntPrimed, double x, double y, double z, float partialTicks) {
         if (tntPrimed.fuse < 1) return;
-        double d0 = tntPrimed.getDistanceSqToEntity(tntRenderer.getRenderManager().livingPlayer);
+        double distance = tntPrimed.getDistanceSqToEntity(tntRenderer.getRenderManager().livingPlayer);
 
-        if (d0 <= (double) (64 * 64)) {
+        if (distance <= 4096D) {
             // todo: support bedwars obscure change in time
             float number = (tntPrimed.fuse - partialTicks) / 20F;
-            String str = new DecimalFormat("0.00").format(number);
+            String str = this.format.format(number);
             FontRenderer fontrenderer = tntRenderer.getFontRendererFromRenderManager();
-            float f = 1.6F;
-            float f1 = 0.016666668F * f;
             GlStateManager.pushMatrix();
             GlStateManager.translate((float) x + 0.0F, (float) y + tntPrimed.height + 0.5F, (float) z);
             GL11.glNormal3f(0.0F, 1.0F, 0.0F);
@@ -49,8 +44,9 @@ public class TNTTime {
                 xMultiplier = -1;
             }
 
+            float scale = 0.02666667f;
             GlStateManager.rotate(tntRenderer.getRenderManager().playerViewX * xMultiplier, 1.0F, 0.0F, 0.0F);
-            GlStateManager.scale(-f1, -f1, f1);
+            GlStateManager.scale(-scale, -scale, scale);
             GlStateManager.disableLighting();
             GlStateManager.depthMask(false);
             GlStateManager.disableDepth();
@@ -58,21 +54,20 @@ public class TNTTime {
             GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
             Tessellator tessellator = Tessellator.getInstance();
             WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-            int i = 0;
-            int j = fontrenderer.getStringWidth(str) / 2;
+            int stringWidth = fontrenderer.getStringWidth(str) >> 1;
             float green = Math.min(tntPrimed.fuse / 80f, 1f);
             Color color = new Color(1f - green, green, 0f);
             GlStateManager.enableDepth();
             GlStateManager.depthMask(true);
             GlStateManager.disableTexture2D();
             worldrenderer.begin(7, DefaultVertexFormats.POSITION_COLOR);
-            worldrenderer.pos(-j - 1, -1 + i, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-            worldrenderer.pos(-j - 1, 8 + i, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-            worldrenderer.pos(j + 1, 8 + i, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-            worldrenderer.pos(j + 1, -1 + i, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+            worldrenderer.pos(-stringWidth - 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+            worldrenderer.pos(-stringWidth - 1, 8, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+            worldrenderer.pos(stringWidth + 1, 8, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+            worldrenderer.pos(stringWidth + 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
             tessellator.draw();
             GlStateManager.enableTexture2D();
-            fontrenderer.drawString(str, -fontrenderer.getStringWidth(str) / 2, i, color.getRGB());
+            fontrenderer.drawString(str, -fontrenderer.getStringWidth(str) >> 1, 0, color.getRGB());
             GlStateManager.enableLighting();
             GlStateManager.disableBlend();
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
